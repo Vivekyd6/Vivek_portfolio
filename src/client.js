35f3cae4121b9a0054c23bbbeeef5de8b312c0f8
Vivek Yadav <vivekyadav36837@gmail.com>
2022-07-22 09:17:45 +0530

    dotenv add

--- src/client.js
+++ src/client.js
@@ -1,6 +1,10 @@
 import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
+
+require('dotenv').config();
+
+
 export const client = sanityClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
